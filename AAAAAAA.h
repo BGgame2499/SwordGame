f40cea875c34abcae80f6f6d7bbbc014420db8e6
BGgame2499 <2499595959@qq.com>
2018-04-14 12:18:06 +0800

    Test

---
+++ AAAAAAA.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwordGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWORDGAME_API ASwordGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
